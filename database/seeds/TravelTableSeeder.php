08aced0334b9aca2d3360b89ccee8bbfaa1f569f
daviddpi <piscopodavid97@gmail.com>
2021-11-12 18:07:21 +0100

    fixed bug seeder faker

--- database/seeds/TravelTableSeeder.php
+++ database/seeds/TravelTableSeeder.php
@@ -2,6 +2,7 @@
 
 use App\Travel;
 use Faker\Provider\Lorem;
+use Faker\Generator as Faker;
 use Illuminate\Database\Seeder;
 
 class TravelTableSeeder extends Seeder
@@ -11,38 +12,17 @@ class TravelTableSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        $travels = [
+        for ($i = 0; $i < 10; $i++){
+            $travel = new Travel();
+            $travel->place = $faker->sentence(1);
+            $travel->country = $faker->sentence(3);
+            $travel->description = $faker->paragraph(3);
+            $travel->price = $faker->randomNumber(4, true);
+            $travel->image_url = $faker->imageUrl(300, 200, $travel->place, $travel->price);
 
-            [
-                "place" => "Los Angeles",
-                "country" => "California",
-                "description" => "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquid, sequi nesciunt velit porro vero dolore et, laborum sint maiores quod sapiente! Repellendus a eaque deserunt et earum dicta quisquam natus.",
-                "price" => 890,
-                "image_url" => "https://a.travel-assets.com/findyours-php/viewfinder/images/res70/475000/475457-Los-Angeles.jpg"
-            ],
-
-            [
-                "place" => "New York",
-                "country" => "New York",
-                "description" => "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquid, sequi nesciunt velit porro vero dolore et, laborum sint maiores quod sapiente! Repellendus a eaque deserunt et earum dicta quisquam natus.",
-                "price" => 1780,
-                "image_url" => "https://www.corsiperbarman.it/wp-content/uploads/2021/03/lavorare-a-new-york.jpg"
-            ]
-        ];
-
-        foreach( $travels as $travel){
-            $newTravel = new Travel();
-            $newTravel->place = $travel["place"];
-            $newTravel->country = $travel["country"];
-            $newTravel->description = $travel["description"];
-            $newTravel->price = $travel["price"];
-            $newTravel->image_url = $travel["image_url"];
-
-            $newTravel->save();
-
-
+            $travel->save();
         }
     }
 }
